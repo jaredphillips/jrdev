47a2175197a261dd22e3bc5eeda1e68badf94e4e
Jared Phillips <jaredtphillips@me.com>
2014-05-25 21:54:26 -0400

    cleaned up unneeded line in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,6 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      client.update(@article.twitter_info)
       redirect_to new_article_path, notice: 'Article was successfully created.'
     else
       render action: 'new'
